0701433964ca5967ba30e90740dac1d4c4aeac9c
WemmittEmily <wemmitte9824@student.faytechcc.edu>
2025-06-15 23:23:14 -0400

    P1HWI_WemmittEmily

---
+++ P1HW1_WemmittEmily.py
@@ -0,0 +1,26 @@
+#Emily Wemmitt
+#6/15/2025
+#P1WH1
+#Write a Python program that uses mathematical expressions
+
+print("-----Calculating Exponents-----")
+
+
+
+base = int(input("Enter an integer as the base value:"))
+exponent = int(input("Enter an integer as the exponent:"))
+
+
+
+print(base, "raised to the power of", exponent, "is", base ** exponent, "!!!")
+
+
+print("-----Addition and Subtraction-----")
+
+
+
+start = int(input("Enter a starting integer:"))
+to_add = int(input("Enter an integer to add:"))
+to_subtract = int(input("Enter an integer to subtract:"))
+
+print(start, "+", to_add, "-", to_subtract, "is equal to", start + to_add - to_subtract)
