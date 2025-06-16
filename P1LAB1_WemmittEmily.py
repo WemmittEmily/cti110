4b0d11891ce80edbb51db50dda60744386b8e904
WemmittEmily <wemmitte9824@student.faytechcc.edu>
2025-06-15 22:07:26 -0400

    First Submission

---
+++ P1LAB1_WemmittEmily.py
@@ -0,0 +1,13 @@
+#Emily Wemmitt
+#6/12/2025
+#P1LAB1
+#Create a program that allows the user to enter their first name and last name.
+
+
+first_name = input("Enter your first name:")
+
+last_name = input("Enter your last name:")
+
+print("Hello,", first_name, last_name,"!", "Welcome to CTI-110")
+
+    
