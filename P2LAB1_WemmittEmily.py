664bc82f60c6ffcd55a9edf493552a50dd6c3d73
WemmittEmily <wemmitte9824@student.faytechcc.edu>
2025-06-23 15:21:00 -0400

    P2LAB1

---
+++ P2LAB1_WemmittEmily.py
@@ -0,0 +1,13 @@
+#Emily Wemmitt
+#6/23/2025
+#P2LAB1
+# how to write code that performs mathematical calculations and displays information to users
+
+pi = (3.14159265359)
+radius_of_circle = float(input("What is the radius of the circle?"))
+
+print(f"The diameter of the circle is {2*radius_of_circle:.1f}")
+
+print(f"The circumference of the circle is {2*pi*radius_of_circle:.2f}")
+
+print(f"The area of the circle is {pi*(radius_of_circle**2):.3f}")
