1fafac9fabefb7973d911bbc2d893f1ab79ffce9
WemmittEmily <wemmitte9824@student.faytechcc.edu>
2025-07-20 12:51:19 -0400

    P2HW2

---
+++ P2HW2_WemmittEmily.py
@@ -0,0 +1,52 @@
+#Emily Wemmitt
+#6/23/25
+#P2HW2
+#Design a program to show grades
+
+
+#------------------------------
+#Puesdo code:
+#Enter test grades for Module 1 thru Module 6
+#Put all grades in a list named "module_grades"
+#Calculate the lowest grade, the highest grade, sum of all grades and the average of grades
+#Display results
+#------------------------------
+
+
+num_scores = int(input("How many scores would you like to enter? "))
+
+module_grades = []
+
+while len(module_grades) < num_scores:
+    score = float(input(f"Enter Grade #{len(module_grades) + 1}: "))
+    if 0 <= score <= 100:
+        module_grades.append(score)
+    else:
+        print("INVALID Score entered!!!! Score should be between 0 and 100")
+
+original_grades = []
+for grade in module_grades:
+    original_grades.append(grade)
+
+lowest = min(module_grades)
+module_grades.remove(lowest)
+
+average = sum(module_grades) / len(module_grades)
+
+if average >= 90:
+    grade = "A"
+elif average >= 80:
+    grade = "B"
+elif average >= 70:
+    grade = "C"
+elif average >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print("----------Results----------")
+print(f"{"Lowest Grade:":20} {lowest}")
+print(f"{"Modified List:":20} {module_grades}")
+print(f"{"Scores Average:":20} {average:.2f}")
+print(f"{"Letter Grade:":20} {grade}")
+print("---------------------------")
